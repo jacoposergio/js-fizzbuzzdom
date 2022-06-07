3f90064fcd8e30769d146e894d49246238aa276b
jacoposergio <djago32@gmail.com>
2022-06-07 15:49:25 +0200

    fizzbuzzcolor

--- MILESTONE 1/js/script.js
+++ MILESTONE 1/js/script.js
@@ -1,1 +1,37 @@
-alert("Hello! I am an alert box!!");
+// MILESTONE 3
+// Coloriamo i numeri pari 
+// in rosso e i dispari in verde nell'html
+
+// const numbersContainer = document.querySelector('.numbers-container');
+
+// for(let i = 0; i <= 100; i++) {
+//     // Assegniamo ad una variabile il nome della classe per il colore
+//     // Se è pari .even, altrimenti .odd
+//     let evenOddClassName;
+//     if(i % 2 === 0) {
+//         evenOddClassName = 'even';
+//     } else {
+//         evenOddClassName = 'odd';
+//     }
+
+    
+const numbersContainer = document.querySelector('.numbers-container');
+
+for(let i = 0; i <= 100; i++) {
+    // Assegniamo ad una variabile il nome della classe per il colore
+    // Se è pari .even, altrimenti .odd
+    let threeFiveClassName;
+    if(i % 5 === 0 && i % 3 === 0) {
+        threeFiveClassName = 'fizzbuzz';
+    } else if(i % 3 === 0 ){
+        threeFiveClassName = 'fizz'
+    } else if(i % 5 === 0 ){
+        threeFiveClassName = 'buzz'
+    }
+
+    // Creare dei div box
+    const newBox = `<div class="box ${threeFiveClassName}">${i}</div>`;
+
+    // Li concateniamo a numbersContainer
+    numbersContainer.innerHTML += newBox;
+}
