c6369e859c4d491f4e69f16cbfd92b040aa9a339
jacoposergio <djago32@gmail.com>
2022-06-07 16:32:21 +0200

    stampare numeri

--- MILESTONE 1/js/script.js
+++ MILESTONE 1/js/script.js
@@ -1,19 +1,3 @@
-// MILESTONE 3
-// Coloriamo i numeri pari 
-// in rosso e i dispari in verde nell'html
-
-// const numbersContainer = document.querySelector('.numbers-container');
-
-// for(let i = 0; i <= 100; i++) {
-//     // Assegniamo ad una variabile il nome della classe per il colore
-//     // Se è pari .even, altrimenti .odd
-//     let evenOddClassName;
-//     if(i % 2 === 0) {
-//         evenOddClassName = 'even';
-//     } else {
-//         evenOddClassName = 'odd';
-//     }
-
     
 const numbersContainer = document.querySelector('.numbers-container');
 
@@ -30,8 +14,11 @@ for(let i = 0; i <= 100; i++) {
     }
 
     // Creare dei div box
-    const newBox = `<div class="box ${threeFiveClassName}">${i}</div>`;
+    const newBox = `<div class=" box ${threeFiveClassName}">${i}</div>`;
 
     // Li concateniamo a numbersContainer
     numbersContainer.innerHTML += newBox;
+
+  
 }
+
